075fb6be7654520624ef2da6b07e4c0ce464d798
AYYAZmayo <43933912+AYYAZmayo@users.noreply.github.com>
2020-06-28 19:58:53 +0500

    Update monitor_dummy.sv

--- monitor_dummy.sv
+++ monitor_dummy.sv
@@ -1,34 +1,41 @@
-
 class monitor;
-	int x;
-	int y;
-	virtual intf vif;
 	mailbox mon2scb;
-
-	function new(virtual intf vif,mailbox mon2scb);
-		this.vif = vif;
+	int no_ack;
+	int no_err;
+	virtual wb_interface interf;      //interface                  
+	function new(wb_interface interf,mailbox mon2scb);
+		this.interf = interf;
 		this.mon2scb = mon2scb;
 	endfunction
 
-	task remove;
-		transaction trans;
-		mon2scb.get(trans);
-	endtask
-
 	task main;
 		transaction trans;
-		trans = new(0, x, 0);
-		@(posedge vif.clk);
-		vif.rd_en <= 1;
-		vif.addr  <= trans.addr;
-		@(posedge vif.clk);
-		trans.rdata <= vif.rdata;
-		y <= vif.rdata;
-		@(posedge vif.clk);
-		vif.rd_en <= 0;
+		trans = new();
+	    @(posedge interf);
+			if(interf.wr_i && interf.stb_i)begin			/// write 
+				@(posedge interf);
+				wait(interf.ack_o||err_o);
+				if (interf.ack_o) no_ack++;
+				else no_err++;
+				trans.dat_in      <= interf.dat_i;     
+				trans.select_bank <= interf.sel_i;
+				trans.address     <= interf.adr_i;
+				trans.write_enable<= interf.wr_i;
+				end
+			else if( !interf.wr_i && interf.stb_i)begin // read
+				@(posedge interf);
+				wait(interf.ack_o||err_o);
+				if (interf.ack_o) no_ack++;
+				else no_err++;
+				trans.dat_out     <= interf.dat_o;     
+				trans.select_bank <= interf.sel_i;
+				trans.address     <= interf.adr_i;
+				trans.write_enable<= interf.wr_i;
+				end
 		mon2scb.put(trans);
-		trans.display_read("[ Monitor Read ]");
+		$display("[ Monitor Read ]");
 	endtask
 
 endclass
+
 
