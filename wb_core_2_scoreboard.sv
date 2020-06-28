2a9a825031000f192cab72b357e565caa9497b96
AYYAZmayo <43933912+AYYAZmayo@users.noreply.github.com>
2020-06-29 00:25:17 +0500

    Update wb_core_2_scoreboard.sv

--- wb_core_2_scoreboard.sv
+++ wb_core_2_scoreboard.sv
@@ -2,21 +2,38 @@
 class scoreboard;
 	int no_of_errors;
 	mailbox mon2scb;
-
-	function new(mailbox mon2scb);
+	mailbox gen2scb;
+    int transaction_count;	//transaction counter
+	transaction gtrans,mtrans;
+	bit [32:0] mem[16];
+	function new(mailbox mon2scb,mailbox gen2scb);
 		this.mon2scb = mon2scb;
+		this.gen2scb = gen2scb;
 	endfunction
-
+    task collect;
+		repeat (transaction_count) begin
+			gen2scb.get(gtrans);
+			if(gtrans.wr_i) 
+            mem[gtrans.adr_i] = gtrans.dat_i;
+			end
+			
 	task main;
-		transaction trans;
-		mon2scb.get(trans);
-		if(trans.wdata == trans.rdata)
-			$display("Result is as Expected");
-		else begin
-			no_of_errors = no_of_errors + 1;     
-			$error("Wrong Result.\n\tExpeced: %0d Actual: %0d", trans.wdata, trans.rdata);
-		end
+		repeat (transaction_count) begin
+		mon2scb.get(mtrans);
+		if(!mtrans.wr_i) begin
+			if(mem[gtrans.adr_i] != mtrans.dat_i); 
+            $error("[SCB-FAIL] Addr = %0h,\n\t Data :: Expected = %0h Actual = %0h",gtrans.adr_i,mem[gtrans.adr_i],mtrans.dat_i);
+        else 
+          $display("[SCB-PASS] Addr = %0h,\n \tData :: Expected = %0h Actual = %0h",gtrans.adr_i,mem[gtrans.adr_i],mtrans.dat_i);
+      end
 	endtask
-
+    task run;
+	fork 
+	collect;
+	main;'
+	join
+    end
 endclass
+
+
 
