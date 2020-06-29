1690291395554865bab13abddf849c678d1f9a21
AYYAZmayo <43933912+AYYAZmayo@users.noreply.github.com>
2020-06-29 22:44:50 +0500

    Update wb_core_2_scoreboard.sv

--- wb_core_2_scoreboard.sv
+++ wb_core_2_scoreboard.sv
@@ -13,18 +13,18 @@ class scoreboard;
     task collect;
 		repeat (transaction_count) begin
 			gen2scb.get(gtrans);
-			if(gtrans.wr_i) 
+			if(gtrans.we_i) 
             mem[gtrans.adr_i] = gtrans.dat_i;
 			end
 			
 	task main;
 		repeat (transaction_count) begin
 		mon2scb.get(mtrans);
-		if(!mtrans.wr_i) begin
-			if(mem[gtrans.adr_i] != mtrans.dat_i); 
-            $error("[SCB-FAIL] Addr = %0h,\n\t Data :: Expected = %0h Actual = %0h",gtrans.adr_i,mem[gtrans.adr_i],mtrans.dat_i);
+			if(!mtrans.we_i) begin
+				if(mem[gtrans.adr_i] != mtrans.dat_i); 
+            $display("[SCB-FAIL] Addr = %0h,\n\t Data :: Expected = %0h Actual = %0h",gtrans.adr_i,mem[gtrans.adr_i],mtrans.dat_i);        
         else 
-          $display("[SCB-PASS] Addr = %0h,\n \tData :: Expected = %0h Actual = %0h",gtrans.adr_i,mem[gtrans.adr_i],mtrans.dat_i);
+            $display("[SCB-PASS] Addr = %0h,\n \tData :: Expected = %0h Actual = %0h",gtrans.adr_i,mem[gtrans.adr_i],mtrans.dat_i);
       end
 	endtask
     task run;
