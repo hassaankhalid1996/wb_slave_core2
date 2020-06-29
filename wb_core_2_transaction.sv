6f315890d9c456aba65dc475b8653dcc9608751d
ahmedjunaid339 <67510597+ahmedjunaid339@users.noreply.github.com>
2020-06-29 23:41:12 +0500

    Update wb_core_2_transaction.sv

--- wb_core_2_transaction.sv
+++ wb_core_2_transaction.sv
@@ -12,10 +12,15 @@ class transaction;
 	address >= 0;
 	};
 	
-	function new ();
-	$display("address = %d, data_out = %d, data_in = %d, select_bank = %d, bitwrite_enable = %d",
-	address, data_out, data_in, select_bank, write_enable);
+
+	function new (bit [15:0]address, bit [31:0]data_out, bit [31:0]data_in, bit [3:0]select_bank, bit write_enable);
+		this.address = address;
+		this.data_in = data_in; 
+		this.select_bank = select_bank; 
+		this.write_enable = write_enable;
+		this.data_out = data_out;
+		$display("address = %d, data_out = %d, data_in = %d, select_bank = %d, bitwrite_enable = %d",
+		address, data_out, data_in, select_bank, write_enable);
 	endfunction
 	
-   
 endclass
