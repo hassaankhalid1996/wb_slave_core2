8854fa19a81e542f8d19b199433bdf1a67718577
Faysal04 <67510901+Faysal04@users.noreply.github.com>
2020-06-29 20:43:50 +0500

    Update generator_dummy.sv

--- generator_dummy.sv
+++ generator_dummy.sv
@@ -1,21 +1,70 @@
 
-class generator;
-	transaction trans;
-	rand int x;
-	rand int y;
-	mailbox gen2driv;
-
-	constraint c1 {x > 0;
-				   x < 2**8;}
-
-	function new(mailbox gen2driv);
-		this.gen2driv = gen2driv;
+class generator; 
+	 
+	mailbox gen2drv;
+	mailbox gen2scb; 
+	
+	function new (mailbox gen2drv , mailbox gen2scb);
+		this.gen2drv = gen2drv;
+		this.gen2scb = gen2scb; 
 	endfunction
+	
+	task SS_pipelined_write;	// single cycle pipelined_write at a specific address 
+		
+		transaction tr; 
+		tr = new();
+		tr.randomize();
+		tr.address = 16'h4;
+		tr.write_enable = 1'b1; 
+		
+		gen2drv.put(tr);
+		gen2scb.put(tr);
+		
+	endtask 		
+		
+	task SS_pipelined_read; // signle cycle pipeline_read at the above defined specific address 
+		transaction tr; 
+		tr = new();
+		tr.address = 16'h4;
+		tr.write_enable = 1'b0;
+		
+		gen2drv.put(tr);
+		gen2scb.put(tr);
+		
+	endtask 
+	
+	task block_write (int transaction_count); // block write
+	
+		for (int i = 0; i < transaction_count ; i++)
+		begin
+		
+			transaction tr; 
+			tr = new();
+			tr.randomize();
+			tr.address = i;
+			tr.write_enable = 1'b1; 
+			gen2drv.put(tr);
+			gen2scb.put(tr);
+			
+		end 
 
-	task main();
-		trans = new(1, x, y);
-		gen2driv.put(trans);
-	endtask
+	enstask 
+	
+	task block_read (int transaction_count); // block read
+	
+		for (int i = 0; i < transaction_count ; i++)
+		begin
+		
+			transaction tr; 
+			tr = new();
+			tr.address = i;
+			tr.write_enable = 1'b0; 
+			gen2drv.put(tr);
+			gen2scb.put(tr);
+			
+		end 
 
+	enstask 
+		
 endclass
 
