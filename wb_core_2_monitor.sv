18328918b97d32f35d86c62bd48615448ce3db1a
AYYAZmayo <43933912+AYYAZmayo@users.noreply.github.com>
2020-06-29 22:45:45 +0500

    Update wb_core_2_monitor.sv

--- wb_core_2_monitor.sv
+++ wb_core_2_monitor.sv
@@ -3,8 +3,8 @@ class monitor;
 	mailbox mon2scb;
 	int no_ack;
 	int no_err;
-	virtual wb_interface interf;      //interface                  
-	function new(wb_interface interf,mailbox mon2scb);
+	virtual wb_core_2_interface interf;      //interface                  
+	function new(wb_core_2_interface interf,mailbox mon2scb);
 		this.interf = interf;
 		this.mon2scb = mon2scb;
 	endfunction
@@ -16,8 +16,8 @@ class monitor;
 			if(interf.we_i && interf.stb_i&& interf.cyc_i)begin			/// write 
 				@(posedge interf);
 				wait(interf.ack_o||err_o);
-				if (interf.ack_o) no_ack++;
-				else no_err++;
+			    if (`moniter.stall_o)
+				repeat(2) @(posedge interf);
 				trans.dat_in      <= `monitor.dat_i;        
 				trans.select_bank <= `monitor.sel_i;
 				trans.address     <= `monitor.adr_i;
@@ -26,8 +26,8 @@ class monitor;
 			else if( !interf.we_i && interf.stb_i && interf.cyc_i)begin // read
 				@(posedge interf);
 				wait(interf.ack_o||err_o);
-				if (interf.ack_o) no_ack++;
-				else no_err++;
+				if (`moniter.stall_o)
+				repeat(2) @(posedge interf);
 				trans.dat_out     <= `monitor.dat_o;     
 				trans.select_bank <= `monitor.sel_i;
 				trans.address     <= `monitor.adr_i;
@@ -37,5 +37,7 @@ class monitor;
 		$display("[ Monitor Read ]");
 	endtask
 endclass
+
+
 
 
