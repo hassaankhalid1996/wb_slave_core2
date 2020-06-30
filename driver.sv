4f6afc34be5bfca99f8a7b9e2628f0dd38c28453
ahmedjunaid339 <67510597+ahmedjunaid339@users.noreply.github.com>
2020-07-01 01:11:43 +0500

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -7,7 +7,7 @@ class driver;
 	virtual wb_interface interf;      //interface 
 	mailbox gen2driv;                 //A handle for mailbox object  
     
-	function new(mailbox gen2driv, virtual wb_interface interf);
+	function new(virtual wb_interface interf, mailbox gen2driv);
 	    this.interf = interf;
 	    this.gen2driv = gen2driv;
 	endfunction
@@ -32,8 +32,8 @@ class driver;
 	endtask
 
 
-///////////////////////////////////////Standard Single Cycle Read and Write//////////	
-	task SC_Standard;                     //single cycle read and write task
+/////////////////////////////////Standard Single Cycle Read and Write///////////////	
+	task sc_st_rw;              
 		transaction trans;
 		gen2driv.get(trans);
 		`driver.adr_i  <= trans.address;
@@ -43,33 +43,59 @@ class driver;
 		`driver.cyc_i  <= 1'b1;
 		if(trans.write_enable)
 			`driver.dat_i  <= trans.data_out; 
-		
-$display("Driver: Address:%d,select bank:%d,data_out= %d",trans.address,trans.select_bank, trans.data_out);
+
+$display("Driver: Address:%d,select bank:%d,data_out= %d",trans.address,trans.select_bank,trans.data_out);
 
 		wait(`driver.ack_o == 1 || `driver.err_o == 1);   
 
-
-		@(posedge interf.clk_i);
 		`driver.we_i   <= 1'b0;
 		`driver.stb_i  <= 1'b0;
 		`driver.cyc_i  <= 1'b0;
+		@(posedge interf.clk_i);
 	endtask
+///////////////////////////////////Standard Single Cycle Read and Write End/////////
 
 
-	task SC_Standard_Multi_Trans;    //Single Cycle Standard call for multiple transfers(block)
-  		for(i=0;i<transaction_count;i++)
+
+///////////////////////////////////Standard Single Cycle Block Read and Write///////
+
+	task bl_st_rw;  
+		transaction trans;  	
+		`driver.stb_i  <= 1'b1;
+		`driver.cyc_i  <= 1'b1;	
+		
+		for(i=0;i<transaction_count;i++)
 		begin
-			SC_Standard();
+ 			gen2driv.get(trans);
+			`driver.adr_i  <= trans.address;
+			`driver.sel_i  <= trans.select_bank;
+			`driver.we_i   <= trans.write_enable;
+			if(trans.write_enable)
+				`driver.dat_i  <= trans.data_out; 
+
+$display("Driver: Address:%d,select bank:%d,data_out= %d",trans.address,trans.select_bank,trans.data_out);
+
+			wait(`driver.ack_o == 1 || `driver.err_o == 1);   
+
+			`driver.we_i   <= 1'b0;
+			`driver.stb_i  <= 1'b0;
 			@(posedge interf.clk_i);
-			
 		end
+		
+		`driver.cyc_i  <= 1'b0;
 	endtask
+
 //////////////////////////////////Standard Single Cycle Read and Write Ended///////////
 
 
-    
+
+
+
+
+
+
 ///////////////////////////////////////Single Cycle Pipelined Read and Write//////////	
-	task SC_pipelined;                     //single cycle pipelined read and write task
+	task sc_pl_rw;                
 		transaction trans;
 		gen2driv.get(trans);
 		`driver.adr_i  <= trans.address;
@@ -79,8 +105,8 @@ $display("Driver: Address:%d,select bank:%d,data_out= %d",trans.address,trans.se
 		`driver.cyc_i  <= 1'b1;
 		if(trans.write_enable)
 			`driver.dat_i  <= trans.data_out; 
-		
-$display("Driver: Address:%d,select bank:%d,data_out= %d",trans.address,trans.select_bank, trans.data_out);		
+
+$display("Driver: Address:%d,select bank:%d,data_out= %d",trans.address,trans.select_bank,trans.data_out);
 		
 		@(posedge interf.clk_i); 
 		`driver.stb_i <= 1'b0;
@@ -90,67 +116,27 @@ $display("Driver: Address:%d,select bank:%d,data_out= %d",trans.address,trans.se
 		while(`driver.ack_o == 0 && `driver.err_o == 0);  
 		
 		`driver.cyc_i <= 1'b0;
-	endtask
-
-
-	task SC_pipelined_Multi_Trans;        //single cycle pipelined call for multiple transfers
-  		for(i=0;i<transaction_count;i++)
-		begin
-			SC_pipelined();
-			@(posedge interf.clk_i);
-		end
+		@(posedge interf.clk_i);
 	endtask
 //////////////////////////////////Single Cycle Pipelined Read and Write Ended///////////	
 
 
-/////////////////////////////////Block pipelined Read and Write////////////////////////
-	task Block_pipelined_Read;                //Block transfer pipelined Read task 
+
+//////////////////////////////////Block Pipelined Read and Write ///////////////////////
+
+	task bl_pl_rw;               
 		transaction trans;
 		gen2driv.get(trans);	
 
 		@(posedge interf.clk_i);		
 		`driver.stb_i  <= 1'b1;
 		`driver.cyc_i  <= 1'b1;
-		`driver.we_i   <= 1'b0;
 		`driver.adr_i  <= trans.address;
-		`driver.sel_i  <= trans.select_bank;		
-		
-			for(i=1;i<transaction_count;i++)
-			begin
-				gen2driv.get(trans);
+		`driver.sel_i  <= trans.select_bank;
+		`driver.we_i   <= trans.write_enable;		
+		if(trans.write_enable)
+			`driver.dat_i  <= trans.data_out; 
 
-$display("Driver: Address:%d,select bank:%d,data_out= %d",trans.address,trans.select_bank, trans.data_out);
-			
-				@(posedge interf.clk_i);
-				`driver.adr_i  <= trans.address;
-				`driver.sel_i  <= trans.select_bank;
-				while(!(`driver.stall_o))
-				begin
-					@(posedge interf.clk_i);
-					`driver.adr_i  <= trans.address;
-					`driver.sel_i  <= trans.select_bank;
-				end
-			end
-		@(posedge interf.clk_i);
-		`driver.stb_i  <= 1'b0;
-		@(posedge interf.clk_i);
-		`driver.cyc_i  <= 1'b0;
-		
-	endtask
-   
-
-
-	task Block_pipelined_Write;                //Block transfer pipelined Write task 
-		transaction trans;
-		gen2driv.get(trans);	
-
-		@(posedge interf.clk_i);		
-		`driver.stb_i  <= 1'b1;
-		`driver.cyc_i  <= 1'b1;
-		`driver.we_i   <= 1'b1;
-		`driver.adr_i  <= trans.address;
-		`driver.sel_i  <= trans.select_bank;		
-		
 			for(i=1;i<transaction_count;i++)
 			begin
 				gen2driv.get(trans);
@@ -160,6 +146,10 @@ $display("Driver: Address:%d,select bank:%d,data_out= %d",trans.address,trans.se
 				@(posedge interf.clk_i);
 				`driver.adr_i  <= trans.address;
 				`driver.sel_i  <= trans.select_bank;
+				`driver.we_i   <= trans.write_enable;	
+				if(trans.write_enable)
+					`driver.dat_i  <= trans.data_out; 
+				
 				while(!(`driver.stall_o))
 				begin
 					@(posedge interf.clk_i);
@@ -175,6 +165,7 @@ $display("Driver: Address:%d,select bank:%d,data_out= %d",trans.address,trans.se
 	endtask
    
 
+//////////////////////////////////Block Pipelined Read and Write End///////////////////////
 
 endclass
 
