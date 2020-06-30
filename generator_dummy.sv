84082750d36056360768b1e5bed28e36c569754c
Faysal04 <67510901+Faysal04@users.noreply.github.com>
2020-06-30 13:22:00 +0500

    Update generator_dummy.sv

--- generator_dummy.sv
+++ generator_dummy.sv
@@ -1,4 +1,3 @@
-
 class generator; 
 	 
 	mailbox gen2drv;
@@ -9,62 +8,104 @@ class generator;
 		this.gen2scb = gen2scb; 
 	endfunction
 	
-	task SS_pipelined_write;	// single cycle pipelined_write at a specific address 
-		
+	task write_specific_addr(bit [15:0] address);
+	
+/* Description :
+This task is designed to write data at the gien specific address 
+The data written would be equal to address provided 
+*/
+
 		transaction tr; 
-		tr = new();
-		tr.randomize();
-		tr.address = 16'h4;
-		tr.write_enable = 1'b1; 
-		
+		tr = new (address, 0 , address , 4'b1111, 1'b1);
 		gen2drv.put(tr);
 		gen2scb.put(tr);
 		
 	endtask 		
-		
-	task SS_pipelined_read; // signle cycle pipeline_read at the above defined specific address 
+	
+	task read_specific_addr( bit [15:0] address);
+	
+/* Description:
+This task is for reading data from a specific address.
+*/
 		transaction tr; 
-		tr = new();
-		tr.address = 16'h4;
-		tr.write_enable = 1'b0;
-		
+		tr = new (address, 0 , 0 , 4'b1111, 1'b0);
 		gen2drv.put(tr);
 		gen2scb.put(tr);
-		
+	
 	endtask 
 	
-	task block_write (int transaction_count); // block write
-	
-		for (int i = 0; i < transaction_count ; i++)
-		begin
-		
-			transaction tr; 
-			tr = new();
-			tr.randomize();
-			tr.address = i;
-			tr.write_enable = 1'b1; 
-			gen2drv.put(tr);
-			gen2scb.put(tr);
-			
-		end 
+	task full_block_write ();
 
-	enstask 
+/* Description
+This task is to write block data in the slave. 
+The block size would be 16. 
+In this case the data written would also be equal to address provided.
+*/
+	for (int i = 0 ; i < 16 ; i++)
+	begin 
+		transaction tr; 
+		tr = new (i, 0 , i, 4'b1111, 1'b1);
+		gen2drv.put(tr);
+		gen2scb.put(tr);
+	end 	
+	endtask 
 	
-	task block_read (int transaction_count); // block read
-	
-		for (int i = 0; i < transaction_count ; i++)
-		begin
-		
-			transaction tr; 
-			tr = new();
-			tr.address = i;
-			tr.write_enable = 1'b0; 
-			gen2drv.put(tr);
-			gen2scb.put(tr);
-			
-		end 
+	task full_block_read ();
 
-	enstask 
-		
+/* Description
+This task is to read block data in the slave. 
+The block size would be 16.
+*/
+	for (int i = 0 ; i < 16 ; i++)
+	begin 
+		transaction tr; 
+		tr = new (i, 0 , 0, 4'b1111, 1'b0);
+		gen2drv.put(tr);
+		gen2scb.put(tr);
+	end 	
+	endtask 
+	
+	task partial_block_write (int value_less_than_16);
+	
+	/* Description
+This task is to write block data in the slave. 
+The block size would be controlled by environment. 
+In this case the data written would also be equal to address provided.
+*/
+	for (int i = 0 ; i < value_less_than_16 ; i++)
+	begin 
+		transaction tr; 
+		tr = new (i, 0 , i, 4'b1111, 1'b1);
+		gen2drv.put(tr);
+		gen2scb.put(tr);
+	end 	
+	endtask
+	
+	task partial_block_read (int value_less_than_16);
+	
+	/* Description
+This task is to read block data in the slave. 
+The block size would be controlled by environment. 
+*/
+	for (int i = 0 ; i < value_less_than_16 ; i++)
+	begin 
+		transaction tr; 
+		tr = new (i, 0 , 0, 4'b1111, 1'b0);
+		gen2drv.put(tr);
+		gen2scb.put(tr);
+	end 	
+	endtask
+	
+	task write_read_random_addr();
+	
+/* Description
+This task write at a random address
+and then read from the same random address 
+*/
+		bit [15:0] i; 
+		i = $urandom_range(15,0);
+		write_specific_addr(i);
+		read_specific_addr(i);
+	endtask
+	
 endclass
-
