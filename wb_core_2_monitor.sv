2eefb16b7e65c004c986c494d8175a1ecf8af25a
AYYAZmayo <43933912+AYYAZmayo@users.noreply.github.com>
2020-06-30 10:32:53 +0500

    Update wb_core_2_monitor.sv

--- wb_core_2_monitor.sv
+++ wb_core_2_monitor.sv
@@ -3,8 +3,8 @@ class monitor;
 	mailbox mon2scb;
 	int no_ack;
 	int no_err;
-	virtual wb_core_2_interface interf;      //interface                  
-	function new(wb_core_2_interface interf,mailbox mon2scb);
+	virtual wb_interface interf;      //interface                  
+	function new(virtual wb_interface interf,mailbox mon2scb);
 		this.interf = interf;
 		this.mon2scb = mon2scb;
 	endfunction
@@ -12,31 +12,37 @@ class monitor;
 	task main;
 		transaction trans;
 		trans = new();
-	    @(posedge interf);
+	    @(posedge `monitor.clk_i);
 			if(interf.we_i && interf.stb_i&& interf.cyc_i)begin			/// write 
-				@(posedge interf);
-				wait(interf.ack_o||err_o);
-			    if (`moniter.stall_o)
-				repeat(2) @(posedge interf);
-				trans.dat_in      <= `monitor.dat_i;        
+				@(posedge `monitor.clk_i);
+				wait(interf.ack_o);
+			    if (interf.stall_o)
+				repeat(2) @(posedge `monitor.clk_i);
+				trans.data_in      <= `monitor.dat_i;        
 				trans.select_bank <= `monitor.sel_i;
 				trans.address     <= `monitor.adr_i;
 				trans.write_enable<= `monitor.we_i;
 				end
 			else if( !interf.we_i && interf.stb_i && interf.cyc_i)begin // read
-				@(posedge interf);
-				wait(interf.ack_o||err_o);
-				if (`moniter.stall_o)
-				repeat(2) @(posedge interf);
-				trans.dat_out     <= `monitor.dat_o;     
+				@(posedge `monitor.clk_i);
+				wait(interf.ack_o);
+				if (interf.stall_o)
+				repeat(2) @(posedge `monitor.clk_i);
+				trans.data_out     <= `monitor.dat_o;     
 				trans.select_bank <= `monitor.sel_i;
 				trans.address     <= `monitor.adr_i;
 				trans.write_enable<= `monitor.we_i;
 				end
+                                @(posedge `monitor.clk_i);
 		mon2scb.put(trans);
 		$display("[ Monitor Read ]");
 	endtask
 endclass
+
+
+
+
+
 
 
 
