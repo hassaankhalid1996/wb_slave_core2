35184016ac43d9cb3e8b2fe363acb2e495ec6075
AYYAZmayo <43933912+AYYAZmayo@users.noreply.github.com>
2020-07-01 12:55:28 +0500

    Update scoreboard.sv

--- scoreboard.sv
+++ scoreboard.sv
@@ -1,46 +1,34 @@
 
 class scoreboard;
+	//int no_of_errors;
 	mailbox mon2scb;
-	mailbox gen2scb;
+	//mailbox gen2scb;
     int transaction_count;	//transaction counter
-	transaction gtrans,mtrans;
+	
 	bit [32:0] mem[16];
-	function new(mailbox gen2scb, mailbox mon2scb);
+	function new(mailbox mon2scb);
 		this.mon2scb = mon2scb;
-		this.gen2scb = gen2scb;
+		//this.gen2scb = gen2scb;
+                 foreach(mem[i]) mem[i]=32'd0;
 	endfunction
-    task collect;
-		repeat (transaction_count) begin
-			gtrans=new(0,0,0,15,0);
-			gen2scb.get(gtrans);
-			if(gtrans.write_enable) 
-            		mem[gtrans.address] = gtrans.data_in;
-	end
-	endtask
 			
-	task main;
-		repeat (transaction_count) begin
-			mtrans=new(0,0,0,15,0);
-			mon2scb.get(mtrans);
-			if(!mtrans.write_enable) begin
-				if (mem[gtrans.address] != mtrans.data_in)
-            		$display("[SCB-FAIL]  Data :: Expected = %0h Actual = %0h",mem[gtrans.address],mtrans.data_in);        
-        		else 
-            		$display("[SCB-PASS] Data :: Expected = %0h Actual = %0h",mem[gtrans.address],mtrans.data_in);
-         end
-	end
-	endtask
-
-    task run;
-        begin
-			fork 
-				collect();
-				main();
-			join 
+	task run;
+		transaction mtrans;
+	 forever
+		begin
+		     mon2scb.get(mtrans);
+		     if(mtrans.write_enable)  mem[mtrans.address] = mtrans.data_in;
+			
+		     else if(!mtrans.write_enable) begin
+				if (mem[mtrans.address] != mtrans.data_out)
+            			$display("[SCB-FAIL]  Data :: Expected = %0h Actual = %0h",mem[mtrans.address],mtrans.data_out);        
+        			else 
+            			$display("[SCB-PASS] Data :: Expected = %0h Actual = %0h",mem[mtrans.address],mtrans.data_out);
+        		 	end
 		end
-    endtask
+		transaction_count++;
+	endtask
 endclass
-
 
 
 
