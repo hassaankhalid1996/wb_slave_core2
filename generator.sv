41131fa7255bb20e85d10446f65cc71f93f92310
Faysal04 <67510901+Faysal04@users.noreply.github.com>
2020-07-02 18:49:35 +0500

    Update generator.sv

--- generator.sv
+++ generator.sv
@@ -10,6 +10,21 @@ class generator;
 		this.gen2scb = gen2scb; 
 	endfunction
 	
+	task write_specific_addr_rand_data(bit [15:0] address);
+
+/* Description :
+This task is designed to write data at the gien specific address 
+The data written would be random
+You can call read_specific_addr to read the data
+*/	
+		int i = $urandom();
+		transaction tr; 
+		tr = new (address, i , 0 , 4'b1111, 1'b1);
+		gen2drv.put(tr);
+		gen2scb.put(tr);
+	
+	endtask
+	
 	task write_specific_addr(bit [15:0] address);
 	
 /* Description :
@@ -46,7 +61,7 @@ In this case the data written would also be equal to address provided.
 	for (int i = 0 ; i < 16 ; i++)
 	begin 
 		transaction tr; 
-		tr = new (i, i , 0, 4'b0100, 1'b1);
+		tr = new (i, i , 0, 4'b1111, 1'b1);
 		gen2drv.put(tr);
 		gen2scb.put(tr);
 	end 	
@@ -61,7 +76,7 @@ The block size would be 16.
 	for (int i = 0 ; i < 16 ; i++)
 	begin 
 		transaction tr; 
-		tr = new (i, 0 , 0, 4'b0100, 1'b0);
+		tr = new (i, 0 , 0, 4'b1111, 1'b0);
 		gen2drv.put(tr);
 		gen2scb.put(tr);
 	end 	
@@ -77,7 +92,7 @@ In this case the data written would also be equal to address provided.
 	for (int i = 0 ; i < value_less_than_16 ; i++)
 	begin 
 		transaction tr; 
-		tr = new (i, i , 0, 4'b1000, 1'b1);
+		tr = new (i, i , 0, 4'b1111, 1'b1);
 		gen2drv.put(tr);
 		gen2scb.put(tr);
 	end 	
@@ -92,7 +107,7 @@ The block size would be controlled by environment.
 	for (int i = 0 ; i < value_less_than_16 ; i++)
 	begin 
 		transaction tr; 
-		tr = new (i, 0 , 0, 4'b1000, 1'b0);
+		tr = new (i, 0 , 0, 4'b1111, 1'b0);
 		gen2drv.put(tr);
 		gen2scb.put(tr);
 	end 	
